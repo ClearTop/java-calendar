82692546e8ccf1a04b4f66386f94d78d1e721177
cndwj <cndwj@192.168.137.1>
2020-01-13 04:04:14 +0900

    If "-1"or "bigger than 12" such ad 13,14,15... , break! While inputing number

--- src/hyunmo/calendar/Calendar.java
+++ src/hyunmo/calendar/Calendar.java
@@ -22,18 +22,26 @@ public class Calendar {
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		System.out.print("반복횟수를 입력하세요 : ");
-		int repeat = scanner.nextInt();
-
-		for (int i = 0; i < repeat; i++) {
-			System.out.print("달을 입력하세요 : ");
-			int month = scanner.nextInt();
+		int month=1;
+		
+		while(true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if(month==-1) {
+				break;
+			}
+			if(month>12) {
+				System.out.println("**1~12 까지의 숫자를 입력해주세요!**");
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}		
-		System.out.println("Bye~");
+		System.out.println("Have a nice day!");
 //		cal.printSampleCalendar();
 
 		scanner.close();
