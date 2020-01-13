e9326ea2c82a808cfc7d38421959d67365387ab8
cndwj <cndwj@192.168.137.1>
2020-01-14 06:07:17 +0900

    Add Service replacing from calendar to file

--- src/hyunmo/calendar/PlanItem.java
+++ src/hyunmo/calendar/PlanItem.java
@@ -8,9 +8,9 @@ public class PlanItem {
 	public Date planDate;
 	public String detail;
 	public String peoples = "";
-	
+
 	public static Date getDatefromString(String strDate) {
-		Date date=null;
+		Date date = null;
 		try {
 			date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
 		} catch (ParseException e) {
@@ -23,12 +23,18 @@ public class PlanItem {
 		this.planDate = getDatefromString(date);
 		this.detail = detail;
 	}
-	
+
 	public Date getDate() {
 		return planDate;
 	}
 
 	public void addPeople(String name) {
 		peoples += name + ",";
+	}
+
+	public String saveString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String sdate = formatter.format(planDate);
+		return sdate + "," + "\"" + detail + "\"" + "\n";
 	}
 }
