c21a3b387573844d7742c76562e8234a1c8360aa
cndwj <cndwj@192.168.137.1>
2020-01-14 03:41:01 +0900

    Remove String if-else

--- src/hyunmo/calendar/Prompt.java
+++ src/hyunmo/calendar/Prompt.java
@@ -19,22 +19,24 @@ public class Prompt {
 	 * @return 0~6 (0 = Sunday, 6 = Saturday)
 	 */
 	public int parseDay(String week) {
-		if (week.equals("su"))
+		switch (week) {
+		case "su":
 			return 0;
-		else if (week.equals("mo"))
+		case "mo":
 			return 1;
-		else if (week.equals("tu"))
+		case "tu":
 			return 2;
-		else if (week.equals("wd"))
+		case "wd":
 			return 3;
-		else if (week.equals("th"))
+		case "th":
 			return 4;
-		else if (week.equals("fr"))
+		case "fr":
 			return 5;
-		else if (week.equals("sa"))
+		case "sa":
 			return 6;
-		else
+		default:
 			return 0;
+		}
 	}
 
 	public void runPrompt() throws ParseException {
@@ -43,19 +45,27 @@ public class Prompt {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		while (true) {
+		boolean isLoop = true;
+		while (isLoop) {
 			System.out.println("명령 (1, 2, 3, h, q)");
 			String cmd = scanner.next();
-			if (cmd.equals("1"))
+			switch (cmd) {
+			case "1":
 				cmdRegister(scanner, cal);
-			else if (cmd.equals("2"))
-				cmdSearch(scanner, cal);
-			else if (cmd.equals("3"))
-				cmdCal(scanner, cal);
-			else if (cmd.equals("h"))
-				printMenu();
-			else if (cmd.equals("q"))
 				break;
+			case "2":
+				cmdSearch(scanner, cal);
+				break;
+			case "3":
+				cmdCal(scanner, cal);
+				break;
+			case "h":
+				printMenu();
+				break;
+			case "q":
+				isLoop = false;
+				break;
+			}
 		}
 		System.out.println("Thank you. Bye~!");
 		scanner.close();
@@ -102,7 +112,7 @@ public class Prompt {
 		while (true) {
 			String word = s.next();
 			text += word + " ";
-			if(word.endsWith(";")) {
+			if (word.endsWith(";")) {
 				break;
 			}
 		}
